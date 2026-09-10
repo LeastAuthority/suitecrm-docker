42a826e915f2a7f7501d69de8504237691ac9640
Benoit Donneaux <benoit@leastauthority.com>
2026-09-10 08:49:54 +0200

    Add support for secret files - WiP
    Signed-off-by: Benoit Donneaux <benoit@leastauthority.com>

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -33,10 +33,37 @@ fix_perm () {
   chmod +x vendor/bin/*
 }
 
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+# Credits: https://github.com/MariaDB/mariadb-docker/blob/master/docker-entrypoint.sh
+file_env() {
+  local var="$1"
+  local fileVar="${var}_FILE"
+  local def="${2:-}"
+  if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+    echo "[Entrypoint]: Both $var and $fileVar are set (but are exclusive)"
+    exit 1
+  fi
+  local val="$def"
+  if [ "${!var:-}" ]; then
+    val="${!var}"
+  elif [ "${!fileVar:-}" ]; then
+    val="$(< "${!fileVar}")"
+  fi
+  export "$var"="$val"
+  unset "$fileVar"
+}
+
 echo "[Entrypoint]: SuiteCRM init process started."
 
 # Ensure work is done in the state directory
 [ "${__CWD}" = "${SUITECRM_STATE_DIR}" ] || cd "${SUITECRM_STATE_DIR}"
+
+# Load secrets as environment variable from files, if provided
+file_env SUITECRM_DATABASE_PASSWORD
+file_env SUITECRM_ADMIN_PASSWORD
 
 if [ -s suitecrm_version.php ]; then
   fix_perm  
